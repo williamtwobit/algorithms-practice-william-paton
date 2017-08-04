061e5f1d5850ee881555a64637ca01d360057ffb
williamtwobit <william@twobit.cc>
2017-08-04 13:12:15 -0400

    mapping zeros on 2D array

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
-console.log('hello world');
+const arr = [];
+
+function populate(arr, rows, columns){
+  for(let i = 0; i < columns; i++){
+    arr.push([]);
+    for(let j = 0; j < rows; j++){
+      arr[i].push(Math.floor(Math.random() * 10));
+    }
+  }
+  arr[3][4] = 0;
+}
+
+populate(arr,5,5);
+console.log(arr);
+
+function mapZeros(arr){
+  let i = 0;
+  let map = {
+    rows:[],
+    columns:[],
+  };
+  let output = arr;
+
+  while(i < output.length){
+    map.columns[i] = null;
+    let j = 0;
+    while(j < output[i].length){
+      if(output[i][j] === 0){
+        map.columns[i] = true;
+        map.rows[j] = true;
+      }else if(!map.rows[j]){
+        map.rows[j] = null;
+      }
+      j++;
+    }
+    i++;
+  }
+
+  for(let i = 0; i < output.length; i++){
+    if(map.columns[i]){
+      for(let z = 0; z < output[i].length; z++){
+        output[i][z] = 0;
+      }
+    }
+    else{
+      for(let j = 0; j < output[i].length; j++){
+        if(map.rows[j]){
+          output[i][j] = 0;
+        }
+      }
+    }
+  }
+  output[3][2] = 0;
+  console.log('map:', map);
+  console.log('output:', output);
+}
+
+mapZeros(arr);
